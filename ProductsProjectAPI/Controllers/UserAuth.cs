4ce40dd54df340be4f3e5dc67c5a854270baa25f
DavhanaR2H <119650623+DavhanaR2H@users.noreply.github.com>
2025-03-19 18:53:30 +0200

    Add project files.

---
+++ ProductsProjectAPI/Controllers/UserAuth.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsProjectAPI.Controllers
+{
+    public class UserAuth : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login() 
+        {
+            return Ok();
+        }
+
+        public IActionResult Register() 
+        {
+            return Ok();
+        }
+    }
+}
